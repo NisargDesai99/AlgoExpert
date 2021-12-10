fbde673704bc69c54de68bfee199a5143849b466
Nisarg Desai <nisarg.desai599@gmail.com>
2021-12-10 02:24:18 -0600

    Updates for ReverseWordsInString

--- ReverseWordsInString/main.py
+++ ReverseWordsInString/main.py
@@ -1,27 +1,31 @@
 def is_whitespace(char):
-	return char == '\n' or char == '\t' or char == ' '
+	if char == '\n' or char == '\t' or char == ' ':
+		return char
+	else:
+		return ''
+
 
 def reverse_words_in_string(s):
 	reversed_s = ''
-	
-	curr_word_start_idx = 0
-	for i in range(len(s) - 1, -1, -1):
-		whitespace_char = is_whitespace()
-		if not is_whitespace(s[i]):
-			curr_word += s[i]
-			# print(f'curr_word: {curr_word}')
-		else:
-			print(f'found whitespace - curr reversed_s: {reversed_s}; curr_word: {curr_word}')
-			reversed_s = f'{reversed_s}{curr_word}{whitespace_char}'
-			curr_word = ''
+	curr_word_start_idx = len(s)
+	curr_word_end_idx = len(s)
 
-	return reversed_s
+	for i in range(len(s) - 1, -1, -1):
+		whitespace_char = is_whitespace(s[i])
+		if len(whitespace_char) == 0:
+			curr_word_start_idx -= 1
+		else:
+			reversed_s = f'{reversed_s}{s[curr_word_start_idx:curr_word_end_idx]}{whitespace_char}'
+			curr_word_end_idx = i
+			curr_word_start_idx -= 1
+
+	return f'{reversed_s}{s[curr_word_start_idx:curr_word_end_idx]}'
 
 
 s = 'tim is great'
 result = reverse_words_in_string(s)
 print(f'{s}: {result}')
 
-# s2 = 'whitespaces    4'
-# result2 = reverse_words_in_string(s2)
-# print(f'{s2}: {result2}')
+s2 = 'whitespaces    4'
+result2 = reverse_words_in_string(s2)
+print(f'{s2}: {result2}')
