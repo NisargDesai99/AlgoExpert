f51fb9fb6bc10ce13c5482d577667541cab4e18e
Nisarg Desai <nisarg.desai599@gmail.com>
2022-02-22 01:23:53 -0600

    Updates for NumberOfWaysToTraverseGraph

--- NumberOfWaysToTraverseGraph/main.py
+++ NumberOfWaysToTraverseGraph/main.py
@@ -1,8 +1,8 @@
 
 
-def num_ways_to_traverse_graph(width, height, curr_row=0, curr_col=0):
-	print(f'{curr_row}: {height}')
-	print(f'{curr_col}: {width}')
+def num_ways_to_traverse_graph(width, height, curr_row=1, curr_col=1):
+	# print(f'{curr_row}: {height}')
+	# print(f'{curr_col}: {width}')
 	if curr_row == height:
 		return 1
 	if curr_col == width:
@@ -14,6 +14,13 @@ def num_ways_to_traverse_graph(width, height, curr_row=0, curr_col=0):
 	return num_ways
 
 
+def temp(width, height):
+	if width == 1 or height == 1:
+		return 1
+
+	return temp(width-1, height) + temp(width, height-1)
+
+
 import json
 test_cases = json.load(open('./test_cases.json'))
 itr = 0
@@ -21,6 +28,7 @@ for test in test_cases:
 	if itr != 0:
 		continue
 	num_ways = num_ways_to_traverse_graph(test['input']['width'], test['input']['height'])
+	print(f'input: {test["input"]}')
 	print(f'num_ways = {num_ways}')
 	if num_ways == test['expected_output']:
 		print(f'\tPassed')
