508451b87a085914b36ff09dcc44d72a1988b8fd
Nisarg Desai <nisarg.desai599@gmail.com>
2022-03-28 21:09:42 -0500

    "Updates for knapsack"

--- KnapsackProblem/main.py
+++ KnapsackProblem/main.py
@@ -13,7 +13,9 @@ import time
 
 def knapsack_problem(items, capacity):
 	start_time = time.time()
-	res = knapsack_problem_helper(items, capacity, len(items)-1)
+	# res = knapsack_problem_helper(items, capacity, len(items)-1)
+	
+	matrix = [[-1 for j in range(capacity+1)] for i in range(len(items))]
 	print(f'exec time: {time.time() - start_time}')
 	return [res[0], res[1]]
 
@@ -35,38 +37,41 @@ def knapsack_problem_helper(items, capacity, idx):
 		return res_without_item
 
 
-# def knapsack_problem_helper(items, capacity, idx):
+def knapsack_problem_helper_memoization(items, capacity, idx):
+    
+    
+	if capacity == 0 or idx == -1:
+		return [0, []]
+
+	if items[idx][1] <= capacity:
+		res_with_item = knapsack_problem_helper(items, capacity-items[idx][1], idx-1)
+		res_with_item[0] += items[idx][0]
+		res_with_item[1].append(idx)
+
+		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
+
+		return res_with_item if res_with_item[0] > res_without_item[0] else res_without_item
+	else:
+		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
+		return res_without_item
+
+
+
+# def knapsack_problem_helper_tuples(items, capacity, idx):
 # 	if capacity == 0 or idx == -1:
 # 		return 0, []
-#
+
 # 	if items[idx][1] <= capacity:
-# 		res_with_item_temp = knapsack_problem_helper(items, capacity-items[idx][1], idx-1)
+# 		res_with_item_temp = knapsack_problem_helper_tuples(items, capacity-items[idx][1], idx-1)
 # 		res_with_item_temp[1].append(idx)
 # 		res_with_item = (items[idx][0] + res_with_item_temp[0]), res_with_item_temp[1]
-#
-# 		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
-#
-# 		max_item = get_max_item([res_with_item, res_without_item])
-# 		if max_item == res_with_item:
-# 			return res_with_item
-# 		else:
-# 			return res_without_item
-#
+
+# 		res_without_item = knapsack_problem_helper_tuples(items, capacity, idx-1)
+
+# 		return res_with_item if res_with_item[0] > res_without_item[0] else res_without_item
 # 	else:
-# 		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
+# 		res_without_item = knapsack_problem_helper_tuples(items, capacity, idx-1)
 # 		return res_without_item
-
-
-def get_max_item(items):
-	max_value = -sys.maxsize - 1
-	max_idx = -1
-	counter = 0
-	for item in items:
-		if item[0] > max_value:
-			max_value = item[0]
-			max_idx = counter
-		counter += 1
-	return items[max_idx]
 
 
 print(f'\n\n')
