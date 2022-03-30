7317dc05fe0e6403a30e269dfa5302847a227836
Nisarg Desai <nisarg.desai599@gmail.com>
2022-03-29 23:35:13 -0500

    Adds tabulation for KnapsackProblem; Updates KnapsackProblem memoization to make code cleaner

--- KnapsackProblem/main.py
+++ KnapsackProblem/main.py
@@ -1,51 +1,77 @@
 
-
 def knapsack_problem(items, capacity):
-	memo = [[-1 for j in range(capacity+1)] for i in range(len(items))]
-	res = knapsack_problem_helper_memoization(items, capacity, len(items)-1, memo)
+	# memo = [[-1 for j in range(capacity+1)] for i in range(len(items)+1)]		# needed for memoization func
+	res = knapsack_problem_iterative_tabulation(items, capacity)
+	return res
 
-	return [res[0], res[1]]
 
-
-def knapsack_problem_helper(items, capacity, idx):
+def knapsack_problem_recursive(items, capacity, idx):
 	if capacity == 0 or idx == -1:
 		return [0, []]
 
 	if items[idx][1] <= capacity:
-		res_with_item = knapsack_problem_helper(items, capacity-items[idx][1], idx-1)
+		res_with_item = knapsack_problem_recursive(items, capacity-items[idx][1], idx-1)
 		res_with_item[0] += items[idx][0]
 		res_with_item[1].append(idx)
 
-		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
+		res_without_item = knapsack_problem_recursive(items, capacity, idx-1)
 
 		return res_with_item if res_with_item[0] > res_without_item[0] else res_without_item
 	else:
-		res_without_item = knapsack_problem_helper(items, capacity, idx-1)
+		res_without_item = knapsack_problem_recursive(items, capacity, idx-1)
 		return res_without_item
 
 
-def knapsack_problem_helper_memoization(items, capacity, item_idx, memo):
-	if capacity == 0 or item_idx == -1:
+def knapsack_problem_memoization(items, capacity, item_idx, memo):
+
+	if memo[item_idx][capacity] != -1:
+		return memo[item_idx][capacity]
+
+	if capacity == 0 or item_idx == 0:
 		return [0, []]
 
-	if items[item_idx][1] <= capacity:
-		res_with_item = knapsack_problem_helper_memoization(items, capacity-items[item_idx][1], item_idx-1, memo)
-		res_with_item[0] += items[item_idx][0]
-		res_with_item[1].append(item_idx)
+	if items[item_idx-1][1] <= capacity:
+		# added temp var to avoid editing object in memo
+		res_with_item_temp = knapsack_problem_memoization(items, capacity-items[item_idx-1][1], item_idx-1, memo)
+		res_with_item = [res_with_item_temp[0]+items[item_idx-1][0], res_with_item_temp[1] + [item_idx-1]]
 
-		res_without_item = knapsack_problem_helper_memoization(items, capacity, item_idx-1, memo)
+		res_without_item = knapsack_problem_memoization(items, capacity, item_idx-1, memo)
 
 		memo[item_idx][capacity] = res_with_item if res_with_item[0] > res_without_item[0] else res_without_item
 		return memo[item_idx][capacity]
 	else:
-		memo[item_idx][capacity] = knapsack_problem_helper_memoization(items, capacity, item_idx-1, memo)
+		memo[item_idx][capacity] = knapsack_problem_memoization(items, capacity, item_idx-1, memo)
 		return memo[item_idx][capacity]
+
+
+def knapsack_problem_iterative_tabulation(items, capacity):
+	memo = [[[0, []] if i == 0 or j == 0 else None for j in range(capacity+1)] for i in range(len(items)+1)]
+	for i in range(1, len(items)+1):
+		current_value = items[i-1][0]
+		current_weight = items[i-1][1]
+		for j in range(1, capacity+1):
+			if current_weight <= j:
+				updated_value_items = [(current_value + memo[i-1][j-current_weight][0]),
+									   (memo[i-1][j-current_weight][1] + [i-1])]
+				without_item = memo[i-1][j]
+				memo[i][j] = updated_value_items if updated_value_items[0] > without_item[0] else without_item
+			else:
+				memo[i][j] = memo[i-1][j]
+	return memo[len(items)][capacity]
 
 
 print(f'\n\n')
 import json
 test_cases = json.load(open('test_cases.json'))
-# test_case = test_cases[0]
-# print(knapsack_problem(test_case['inputs']['items'], test_case['inputs']['capacity']))
+test_case = test_cases[0]
+# print(test_case['input'])
+# print(knapsack_problem(test_case['input']['items'], test_case['input']['capacity']))
 for test_case in test_cases:
-	print(knapsack_problem(test_case['inputs']['items'], test_case['inputs']['capacity']))
+	if test_case['expected_output'] != [10, [1, 3]]:
+		continue
+	print(test_case['input'])
+	output = knapsack_problem(test_case['input']['items'], test_case['input']['capacity'])
+	print(f'\t{output}')
+	if output == test_case['expected_output']:
+		print(f'\tPassed')
+
