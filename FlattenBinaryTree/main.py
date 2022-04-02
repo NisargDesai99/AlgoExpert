7257d808ce3039692880aa82ba1b24c43242da26
Nisarg Desai <nisarg.desai599@gmail.com>
2022-04-02 15:57:31 -0500

    Adds space optimized solution for FlattenBinaryTree

--- FlattenBinaryTree/main.py
+++ FlattenBinaryTree/main.py
@@ -6,6 +6,32 @@ class BinaryTree:
 		self.right = right
 
 
+# space optimized solution
 def flatten_binary_tree(root):
+	left, right = flatten_binary_tree_helper(root)
+	return left
 
 
+def flatten_binary_tree_helper(root):
+	if root.left is None:
+		left_most = root
+	else:
+		left_subtree_left_most, left_subtree_right_most = \
+			flatten_binary_tree_helper(root.left)
+		connect_nodes(left_subtree_right_most, root)
+		left_most = left_subtree_left_most
+
+	if root.right is None:
+		right_most = root
+	else:
+		right_subtree_left_most, right_subtree_right_most = \
+			flatten_binary_tree_helper(root.right)
+		connect_nodes(root, right_subtree_left_most)
+		right_most = right_subtree_right_most
+
+	return [left_most, right_most]
+
+
+def connect_nodes(left, right):
+	left.right = right
+	right.left = left
