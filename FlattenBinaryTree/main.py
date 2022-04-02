591f591455b709585c7be3897dc42f2fc91d4fac
Nisarg Desai <nisarg.desai599@gmail.com>
2022-04-01 23:21:04 -0500

    Adds scaffold for FlattenBinaryTree

---
+++ FlattenBinaryTree/main.py
@@ -0,0 +1,11 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+	def __init__(self, value, left=None, right=None):
+		self.value = value
+		self.left = left
+		self.right = right
+
+
+def flatten_binary_tree(root):
+
+
