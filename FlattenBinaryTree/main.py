18dfb7441ec80c2fad6f9901ce436fb49504ee60
Nisarg Desai <nisarg.desai599@gmail.com>
2022-04-06 00:30:34 -0500

    Adds solution for FlattenBinaryTree

--- FlattenBinaryTree/main.py
+++ FlattenBinaryTree/main.py
@@ -5,7 +5,28 @@ class BinaryTree:
 		self.left = left
 		self.right = right
 
+		def __repr__(self):
+			return str(self.value)
 
-def flatten_binary_tree(root):
+		def __str__(self):
+			return str(self.value)
 
 
+def get_inorder(tree, list_tree):
+	if tree is None:
+		return []
+	get_inorder(tree.left, list_tree)
+	list_tree.append(tree)
+	get_inorder(tree.right, list_tree)
+	return list_tree
+
+
+# simple solution
+def flattenBinaryTree(root):
+	inorder_list = get_inorder(root, [])
+	for idx in range(len(inorder_list)):
+		if idx <= len(inorder_list) - 2:
+			inorder_list[idx].right = inorder_list[idx+1]
+		if idx >= 1:
+			inorder_list[idx].left = inorder_list[idx-1]
+	return inorder_list[0]
