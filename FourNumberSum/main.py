26560af0143ad5c5a59f710f6877017e5ecb86db
Nisarg Desai <nisarg.desai599@gmail.com>
2022-04-06 00:46:52 -0500

    Adds scaffold for FourNumberSum

---
+++ FourNumberSum/main.py
@@ -0,0 +1,17 @@
+
+
+def four_number_sum(array, target_sum):
+	print(f'{array} | {target_sum}')
+
+
+
+	return []
+
+
+print(f'\n\n\n')
+import json
+test_cases = json.load(open('test_cases.json', mode='r'))
+for test_case in test_cases:
+	input_params = test_case['input']
+	result = four_number_sum(input_params['array'], input_params['targetSum'])
+
