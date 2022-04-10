cc79f4992a31e08ddf17b0b5b2d8576dde5e8ea5
Nisarg Desai <nisarg.desai599@gmail.com>
2022-04-09 19:17:57 -0500

    Adds solution for FourNumberSum

--- FourNumberSum/main.py
+++ FourNumberSum/main.py
@@ -1,9 +1,29 @@
 
-# TODO: add solution
-def four_number_sum(array, target_sum):
-	print(f'{array} | {target_sum}')
+# Avg: O(n^2)
+# Worst: O(n^3) bc we iterate over all pairs in pair_sum_table
+# 	- imagine this case: [-4,-3,-2,-1,1,2,3,4]
+# 		* we might get {0: [[-4,4], [-3,3]...], ...} in the pair_sum_table
 
-	return []
+def four_number_sum(array, target_sum):
+	n = len(array)
+	pair_sum_table = {}
+	quadruplets = []
+
+	for i in range(1, n-1):
+		for j in range(i+1, n):
+			diff = target_sum - (array[i] + array[j])
+			if diff in pair_sum_table:
+				for pair in pair_sum_table[diff]:
+					quadruplets.append(pair + [array[i], array[j]])
+
+		for k in range(0, i):
+			curr_sum = (array[i] + array[k])
+			if curr_sum not in pair_sum_table:
+				pair_sum_table[curr_sum] = [[array[i], array[k]]]
+			else:
+				pair_sum_table[curr_sum].append([array[i], array[k]])
+
+	return quadruplets
 
 
 if __name__ == '__main__':
